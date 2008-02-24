80903a6907c66cf5fe1631d2a940e47bcfd99961
Andy Armstrong <andy@hexten.net>
2008-02-24 23:55:06 +0000

    Tagging 0.10, version bump

--- lib/IPC/ShareLite.pm
+++ lib/IPC/ShareLite.pm
@@ -10,7 +10,7 @@ IPC::ShareLite - Lightweight interface to shared memory
 
 =head1 VERSION
 
-This document describes IPC::ShareLite version 0.10
+This document describes IPC::ShareLite version 0.11
 
 =cut
 
@@ -49,7 +49,7 @@ require AutoLoader;
 
 Exporter::export_ok_tags( 'all', 'lock', 'flock' );
 
-$VERSION = '0.10';
+$VERSION = '0.11';
 
 =head1 SYNOPSIS
 

3064e9c61b5409ab172b0f923fcdc9ec611b1d49
Andy Armstrong <andy@hexten.net>
2008-02-25 12:22:08 +0000

    Tagged 0.11, version bump

--- lib/IPC/ShareLite.pm
+++ lib/IPC/ShareLite.pm
@@ -10,7 +10,7 @@ IPC::ShareLite - Lightweight interface to shared memory
 
 =head1 VERSION
 
-This document describes IPC::ShareLite version 0.11
+This document describes IPC::ShareLite version 0.12
 
 =cut
 
@@ -49,7 +49,7 @@ require AutoLoader;
 
 Exporter::export_ok_tags( 'all', 'lock', 'flock' );
 
-$VERSION = '0.11';
+$VERSION = '0.12';
 
 =head1 SYNOPSIS
 

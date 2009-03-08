f39c2d62d8ec3725d6d48c57acce4511f5e1379a
Andy Armstrong <andy@hexten.net>
2009-03-08 00:36:43 +0000

    Version bump

--- lib/IPC/ShareLite.pm
+++ lib/IPC/ShareLite.pm
@@ -10,7 +10,7 @@ IPC::ShareLite - Lightweight interface to shared memory
 
 =head1 VERSION
 
-This document describes IPC::ShareLite version 0.14
+This document describes IPC::ShareLite version 0.15
 
 =cut
 
@@ -49,7 +49,7 @@ require AutoLoader;
 
 Exporter::export_ok_tags( 'all', 'lock', 'flock' );
 
-$VERSION = '0.14';
+$VERSION = '0.15';
 
 =head1 SYNOPSIS
 
